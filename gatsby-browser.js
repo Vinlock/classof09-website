74d02fe3870fdef083992f512829e30d3f62e9a3
Dak Washbrook <vinlockz@gmail.com>
2019-10-04 07:43:34 -0700

    Add bugsnag

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,23 @@
  */
 
 // You can delete this file if you're not using it
+
+const React = require('react');
+const bugsnag = require('@bugsnag/js');
+const bugsnagReact = require('@bugsnag/plugin-react');
+
+const bugsnagClient = bugsnag({
+  apiKey: process.env.GATSBY_BUGSNAG_API_KEY,
+  releaseState: process.env.GATSBY_BUGSNAG_RELEASE_STAGE,
+});
+bugsnagClient.use(bugsnagReact, React);
+
+const ErrorBoundary = bugsnagClient.getPlugin('react');
+
+exports.wrapRootElement = ({ element }) => {
+  return (
+    <ErrorBoundary>
+      {element}
+    </ErrorBoundary>
+  );
+};
